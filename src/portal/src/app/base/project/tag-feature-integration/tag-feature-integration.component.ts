102d03486c83b9f16e1607177f9682a6be94939b
Alvaro Fernandez <alvarofdez91@hotmail.es>
2022-06-01 17:07:29 +0200

    Add Release 2.5.1 Harbor

---
+++ src/portal/src/app/base/project/tag-feature-integration/tag-feature-integration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tag-feature-integration',
+    templateUrl: './tag-feature-integration.component.html',
+    styleUrls: ['./tag-feature-integration.component.scss'],
+})
+export class TagFeatureIntegrationComponent {
+    constructor() {}
+}
